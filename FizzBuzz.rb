0a69f3c92f65ef6a2aabb9cc5c89029d8d26a658
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2021-06-04 01:46:10 +0000

    コード変更

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,14 +1,19 @@
+def FizzBuzz(num)
+  if (num%3 == 0) && (num%5 == 0)
+    puts "FizzBuzz"
+  elsif num%3 == 0
+    puts "Fizz"
+  elsif num%5 == 0
+    puts "Buzz"
+  else
+    puts "FizzでもBuzzでもありません。"
+  end
+end
+
 puts "FizzBuzzゲームをします。整数値を入力してください。"
 num = gets.to_i
 puts "あなたが入力したのは#{num}です。"
 
 puts "結果は・・・"
-if (num%3 == 0) && (num%5 == 0)
-  puts "FizzBuzz"
-elsif num%3 == 0
-  puts "Fizz"
-elsif num%5 == 0
-  puts "Buzz"
-else
-  puts "FizzでもBuzzでもありません。"
-end
+FizzBuzz(num)
+
