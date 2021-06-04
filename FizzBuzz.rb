16b1bc53a407712f0430f15f57aa50f588bc6e67
EC2 Default User <ec2-user@ip-172-31-25-149.ec2.internal>
2021-06-04 01:47:29 +0000

    コード変更

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,12 +1,12 @@
 def FizzBuzz(num)
   if (num%3 == 0) && (num%5 == 0)
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif num%3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif num%5 == 0
-    puts "Buzz"
+    return "Buzz"
   else
-    puts "FizzでもBuzzでもありません。"
+    return "FizzでもBuzzでもありません。"
   end
 end
 
